01e8407468fe97d1eaac79fb617faf22e8753e71
Cameron Sinko <csinko@gmail.com>
2017-09-03 22:47:03 -0400

    Add @llf26 leaderboard Angular application to repo.  Not yet implemented.

---
+++ leaderboard/src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  ngOnInit(){this.getStuff()}
+  
+  constructor(private http: HttpClient, private modalService: NgbModal){
+  }
+
+  closeResult: string;
+  
+  open(content) {
+
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  people = [];
+  found = [];
+  title = 'the leaderboard';
+  //everyone = {};
+  lookupName = '';
+  getStuff(){        
+    this.http.get('http://hakron.io/arcade/api/nfc').subscribe(data => {
+    
+      //  console.log(data);
+      var result = JSON.stringify(data);
+      this.people = JSON.parse(result);
+    });
+  };
+  nameLookup(content){
+    if(this.lookupName)
+      {
+      var lowercase = this.lookupName.toLowerCase();
+      var uppercaseFirst = this.lookupName[0].toUpperCase() + this.lookupName.slice(1, this.lookupName.length)
+      var allCapital = this.lookupName.toUpperCase();
+      console.log(allCapital)
+      
+      console.log(lowercase);
+      for(let i = 0; i < this.people.length; i++)
+        {
+          //console.log(this.people[i].name.toUpperCase());
+          if(this.people[i].name === this.lookupName || this.people[i].name.toLowerCase() === this.lookupName || this.people[i].name[0].toUpperCase() + this.people[i].name.slice(1, this.people[i].name.length) === this.lookupName || this.people[i].name.toUpperCase() === this.lookupName)
+            {
+            this.found.push(this.people[i]);      
+            }
+            
+        }
+        if(this.found.length > 0)
+          {
+            //console.log(this.found[0].name);
+            this.modalService.open(content).result.then((result) => {
+              this.closeResult = `Closed with: ${result}`;
+              this.found = [];
+            }, (reason) => {
+              this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+              this.found = [];
+            });
+          }
+          else{alert('We didn\'t find anyone with that name.');}
+    }
+    else{alert('Please enter a name');}
+  }
+}
