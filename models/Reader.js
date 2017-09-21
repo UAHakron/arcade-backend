ecd9861db0a50e48fb1d8d267752f38d40e7fe51
Cameron Sinko <csinko@gmail.com>
2017-09-21 02:41:18 -0400

    Add transaction, Reader schemas

---
+++ models/Reader.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+//UserSchema represents a User.  Each user has a name, NFC id (nfc), email, and bits.  
+var ReaderSchema = new Schema({
+	num: {
+		type: Number,
+		required: [true, "Num is required"],
+		unique: true
+	},
+
+    locations: {
+        type: [String]
+        required: [true, "A location is required"]
+    }
+
+},
+    timestamps: true);
+
+ReaderSchema.plugin(uniqueValidator);
+const Reader = mongoose.model('Reader', ReaderSchema);
+module.exports = Reader;
---
+++ models/Transaction.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+//UserSchema represents a User.  Each user has a name, NFC id (nfc), email, and bits.  
+var TransactionSchema = new Schema({
+	nfc: {
+		type: String,
+		required: [true, "ID is required"],
+		unique: true
+	},
+
+	bits:{
+		type: Number,
+		required: [true, "Bits are required"]
+	},
+
+    reader: {
+        type: String,
+        required: [true, "Reader is required"]
+    },
+
+    location: {
+        type: String,
+        required: [true, "Location is required"]
+    }
+
+},
+    timestamps: true);
+
+TransactionSchema.plugin(uniqueValidator);
+const Transaction = mongoose.model('Transaction', TransactionSchema);
+module.exports = Transaction;
